819a9a31df25ead72d0197f92631093fe4ca85e9
AlanJJ <31364777+AlanJJ@users.noreply.github.com>
2018-01-02 15:10:01 +0800

    首次提交

---
+++ WindowsService/LogHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WindowsService
+{
+    /// <summary>
+    /// 日志类（多线程版）
+    /// </summary>
+    public class LogHelper
+    {
+        private string _fileName;
+        private static Dictionary<long, long> lockDic = new Dictionary<long, long>();
+
+        /// <summary>  
+        /// 获取或设置文件名称  
+        /// </summary>  
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        /// <summary>  
+        /// 构造函数  
+        /// </summary>
+        /// <param name="fileName">文件全路径名</param>  
+        public LogHelper(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new Exception("FileName不能为空！");
+            }
+            Create(fileName);
+            _fileName = fileName;
+        }
+
+        /// <summary>  
+        /// 创建文件路径
+        /// </summary>  
+        /// <param name="fileName">文件路径</param>  
+        public void Create(string fileName)
+        {
+            var directoryPath = Path.GetDirectoryName(fileName);
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                throw new Exception("FileName路径错误！");
+            }
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        /// <summary>  
+        /// 写入文本  
+        /// </summary>  
+        /// <param name="content">文本内容</param>
+        /// <param name="newLine">换行标记</param>  
+        private void Write(string content, string newLine)
+        {
+            using (FileStream fs = new FileStream(_fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 8, FileOptions.Asynchronous))
+            {
+                Byte[] dataArray = Encoding.UTF8.GetBytes(content + newLine);
+                bool flag = true;
+                long slen = dataArray.Length;
+                long len = 0;
+                while (flag)
+                {
+                    try
+                    {
+                        if (len >= fs.Length)
+                        {
+                            fs.Lock(len, slen);
+                            lockDic[len] = slen;
+                            flag = false;
+                        }
+                        else
+                        {
+                            len = fs.Length;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        while (!lockDic.ContainsKey(len))
+                        {
+                            len += lockDic[len];
+                        }
+                    }
+                }
+                fs.Seek(len, SeekOrigin.Begin);
+                fs.Write(dataArray, 0, dataArray.Length);
+                fs.Close();
+            }
+        }
+
+        /// <summary>  
+        /// 写入文件内容  
+        /// </summary>  
+        /// <param name="content">内容</param>  
+        public void WriteLine(string content)
+        {
+            this.Write(content, Environment.NewLine);
+        }
+
+        /// <summary>  
+        /// 写入文件内容  不换行
+        /// </summary>  
+        /// <param name="content">内容</param>  
+        public void Write(string content)
+        {
+            this.Write(content, "");
+        }
+    }
+}
---
+++ WindowsService/Service1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        Thread _serviceThread = null;
+
+        /// <summary>
+        /// 启动服务
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            _serviceThread = new Thread(WritenLog)
+            {
+                IsBackground = true
+            };
+            _serviceThread.Start();
+        }
+
+        /// <summary>
+        /// 终止服务
+        /// </summary>
+        protected override void OnStop()
+        {
+            _serviceThread.Abort();
+        }
+
+        const string LogPath = @"D:\ServiceLog\Log.txt";
+        readonly LogHelper _serviceLogHelper = new LogHelper(LogPath);
+        /// <summary>
+        /// 记日志
+        /// </summary>
+        private void WritenLog()
+        {
+            while (true)
+            {
+                string logText = string.Format("现在时间是：{0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+                Thread.Sleep(10000);//线程休眠10s
+                _serviceLogHelper.WriteLine(logText);
+            }
+        }
+    }
+}
